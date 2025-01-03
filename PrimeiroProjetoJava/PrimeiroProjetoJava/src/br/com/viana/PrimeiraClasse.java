8a1ddd65019838e6e8fc4fc7c3f069facc1f661b
vdeviana <gvhenois@gmail.com>
2025-01-03 17:57:51 -0300

    Add files via upload

---
+++ PrimeiroProjetoJava/PrimeiroProjetoJava/src/br/com/viana/PrimeiraClasse.java
@@ -0,0 +1,16 @@
+package br.com.viana;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args[0];
+		
+		if (ambiente.equals("DEV")) {
+			System.out.print("DEV");
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
